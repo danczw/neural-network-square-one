96ee89655724c7d3ad7685d42991d6f6b71c370a
danczw <dev.dc@pm.me>
2021-06-12 14:32:17 +0200

    added accuracy concept

---
+++ concepts/accuracy.py
@@ -0,0 +1,19 @@
+'''
+Calculating Neural Network accuracy
+    - calculation of accuracy by comparing class target with prediction
+'''
+
+import numpy as np
+
+class_target = [0, 1, 1]
+softmax_output_array = np.array([[0.7, 0.1, 0.2],
+                                 [0.5, 0.1, 0.4],
+                                 [0.02, 0.9, 0.08]]) # confidence per class
+
+# get predictions by finding index of highest class confidence
+predictions = np.argmax(softmax_output_array, axis=1)
+
+# accuracy is the mean difference between predicted and "real" (class_target) value
+accuracy = np.mean(predictions == class_target)
+
+print(f"acc: {accuracy}")
