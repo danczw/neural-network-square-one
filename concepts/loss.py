e0f8e16b3c3d7063b99047766e27d32f45ceaac8
danczw <dev.dc@pm.me>
2021-06-12 14:28:58 +0200

    added confidence of class comment

--- concepts/loss.py
+++ concepts/loss.py
@@ -8,7 +8,7 @@ Calculating Neural Network Loss with Categorical Cross-Entropy
 import math
 
 target_output = [1, 0, 0]           # fictional target output: first class is correct
-softmax_output = [0.7, 0.1, 0.2]    # fictional output of softmax activation function of output layer for each class
+softmax_output = [0.7, 0.1, 0.2]    # fictional output of softmax activation function of output layer for each class - confidence per class
 loss = -(math.log(softmax_output[0]) * target_output[0] +
          math.log(softmax_output[1]) * target_output[1] +
          math.log(softmax_output[2]) * target_output[2])
@@ -25,7 +25,7 @@ import numpy as np
 class_target = [0, 1, 1]
 softmax_output_array = np.array([[0.7, 0.1, 0.2],
                                  [0.1, 0.5, 0.4],
-                                 [0.02, 0.9, 0.08]])
+                                 [0.02, 0.9, 0.08]]) # confidence per class
 
 print(softmax_output_array[
     range(len(softmax_output_array)),   # iterate through array
